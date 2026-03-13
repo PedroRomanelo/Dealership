c685afd29d15dabf2f241072d596b9e6848be0c3
Pedro Romanelo <pedro.romanelo@outlook.com>
2026-03-13 17:00:19 -0300

    feat: some vm

--- Model/Request/AddressVM.cs
+++ Model/Request/AddressVM.cs
@@ -1,6 +1,18 @@
-﻿namespace Dealership.Model.Request
+﻿namespace Dealership.Model.Request;
+
+public class UserAddressCreateRequest
 {
-    public class AddressVM
-    {
-    }
+    public int UserId { get; set; }
+    public string Street { get; set; } = string.Empty;
+    public string Number { get; set; } = string.Empty;
+    public string City { get; set; } = string.Empty;
+    public string State { get; set; } = string.Empty;
+}
+
+public class UserAddressUpdateRequest
+{
+    public string Street { get; set; } = string.Empty;
+    public string Number { get; set; } = string.Empty;
+    public string City { get; set; } = string.Empty;
+    public string State { get; set; } = string.Empty;
 }
